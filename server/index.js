ae9d42b93f4adebaed7e4e54ff16f8451cdb868b
joshborup <joshborup@gmail.com>
2018-12-04 15:29:45 -0700

    done for day

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+<<<<<<< HEAD
 const controller = require("./controller.js");
 app.use(bodyParser.json());
 
@@ -16,3 +17,81 @@ app.get("/api/search", controller.searchOnServer);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+=======
+
+app.use(bodyParser.json());
+
+let favorites = [];
+let id = 0;
+
+app.get("/api/favorites", (req, res) => {
+	res.status(200).send(favorites);
+});
+
+app.post("/api/favorites", (req, res) => {
+	const { imageUrl, name } = req.body;
+	const newFavorite = {
+		id: id,
+		imageUrl: imageUrl,
+		name: name.toLowerCase()
+	};
+
+	if (favorites.length < 6) {
+		favorites.push(newFavorite);
+		id++;
+		console.log(favorites);
+		res.status(200).send(favorites);
+	} else {
+		res.status(403).send(
+			"You cant add anymore cards, try deleting one first"
+		);
+	}
+});
+
+app.put("/api/favorites/:id", (req, res) => {
+	const { imageUrl, name } = req.body;
+	const { id } = req.params;
+
+	favorites.forEach((card) => {
+		if (card.id === parseInt(id)) {
+			card.imageUrl = imageUrl;
+			card.name = name.toLowerCase();
+		}
+	});
+
+	res.status(200).send(favorites);
+});
+
+app.delete("/api/favorites/:id", (req, res) => {
+	const { id } = req.params;
+	favorites = favorites.filter((card) => {
+		// console.log("line 51", card.id !== +id);
+		return card.id !== parseInt(id);
+	});
+
+	console.log(favorites);
+	res.status(200).send(favorites);
+});
+
+app.get("/api/search", (req, res) => {
+	const { name } = req.query;
+	let searched = name.toLowerCase();
+	let filteredList = favorites.filter((card) => {
+		console.log(
+			"card.name.includes(searched);",
+			card.name.includes(searched)
+		);
+		console.log("card.name", card.name, name);
+		return card.name.includes(searched);
+	});
+
+	if (name) {
+		res.status(200).send(filteredList);
+	} else {
+		res.status(200).send(favorites);
+	}
+});
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+>>>>>>> solution
